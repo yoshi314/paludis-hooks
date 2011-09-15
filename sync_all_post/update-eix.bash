7453bb45a0a58c8eab071208f70764328d7d19cb
yoshi314 <yoshi314@gmail.com>
2011-09-15 08:36:29 +0200

    eix hook for paludis. scans all active paludis repositories

---
+++ sync_all_post/update-eix.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+[[ -x /usr/bin/eix-update ]] || exit 0
+source ${PALUDIS_EBUILD_DIR}/echo_functions.bash
+ebegin "Updating eix database"
+opts=
+
+for repo in $(cave print-repositories)
+do
+[[ $(cave print-repository-metadata ${repo} | grep format | cut -d= -f 2) == "ebuild" ]] || \
+[[ $(cave print-repository-metadata ${repo} | grep format | cut -d= -f 2) == "e" ]] || continue
+
+location=$(cave print-repository-metadata ${repo} | grep \^location | cut -d= -f2)
+opts="${opts} --add-overlay ${location}"
+done
+
+/usr/bin/eix-update -o ${ROOT}/var/cache/eix-tmp -q ${opts}
+[ -f ${ROOT}/var/cache/eix ] && /usr/bin/eix-diff ${ROOT}/var/cache/eix ${ROOT}/var/cache/eix-tmp
+
+/bin/mv ${ROOT}/var/cache/eix ${ROOT}/var/cache/eix.old
+
+/bin/mv ${ROOT}/var/cache/eix-tmp ${ROOT}/var/cache/eix
